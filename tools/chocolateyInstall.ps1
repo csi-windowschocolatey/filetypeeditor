4b7e117aa37694f388596880cc7e22c5f199851b
DarwinJS <DarwinJS@sanoys.com>
2015-06-28 07:34:13 -0400

    final

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,3 +3,8 @@ $validExitCodes = @(0)
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'ExecutedProgramsList'
+
+Write-Output "********************************************************************"
+Write-Output "*  INSTRUCTIONS: Type `"gfte.exe`" to edit file type associations. *"
+Write-Output "*       More Info: http://www.asoft.be/prod_netver.html            *"
+Write-Output "********************************************************************"
